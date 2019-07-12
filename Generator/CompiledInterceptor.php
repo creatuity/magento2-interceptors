aff655cfafcc8618296521185796e053c7d86cd9
Franciszek Wawrzak <fwawrzak@creatuity.com>
2019-07-12 11:28:49 +0200

    fix for plugins overriden by preference

--- Generator/CompiledInterceptor.php
+++ Generator/CompiledInterceptor.php
@@ -530,7 +530,6 @@ class CompiledInterceptor extends EntityAbstract
             'visibility' => 'private',
             'parameters' => [],
             'body' => implode("\n", $body),
-            'returnType' => $plugin['class'],
             'docblock' => [
                 'shortDescription' => 'plugin "' . $plugin['code'] . '"' . "\n" . '@return \\' . $plugin['class']
             ],
