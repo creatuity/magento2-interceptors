98c84e589f33ae1620326513e1f178cf95f7302b
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2022-03-01 21:38:21 +0100

    restore compiled arguments fix, fix for preference for scope

--- Generator/CompiledInterceptorSubstitution.php
+++ Generator/CompiledInterceptorSubstitution.php
@@ -3,26 +3,12 @@
 namespace Creatuity\Interception\Generator;
 
 use Magento\Setup\Module\Di\Compiler\Config\Chain\InterceptorSubstitution;
-use Magento\Setup\Module\Di\Compiler\Config\ModificationInterface;
 
 /**
  * Class CompiledInterceptorSubstitution adds required parameters to interceptor constructor
  */
-class CompiledInterceptorSubstitution implements ModificationInterface
+class CompiledInterceptorSubstitution extends InterceptorSubstitution
 {
-    /**
-     * @var InterceptorSubstitution
-     */
-    private $interceptorSubstitution;
-
-    /**
-     * @param InterceptorSubstitution $interceptorSubstitution
-     */
-    public function __construct(InterceptorSubstitution $interceptorSubstitution)
-    {
-        $this->interceptorSubstitution = $interceptorSubstitution;
-    }
-
     /**
      * Modifies input config
      *
@@ -31,15 +17,19 @@ class CompiledInterceptorSubstitution implements ModificationInterface
      */
     public function modify(array $config)
     {
-        $config = $this->interceptorSubstitution->modify($config);
+        $config = parent::modify($config);
         foreach ($config['arguments'] as $instanceName => &$arguments) {
             $finalInstance = isset($config['instanceTypes'][$instanceName]) ? $config['instanceTypes'][$instanceName] : $instanceName;
             if (substr($finalInstance, -12) === '\Interceptor') {
                 foreach (CompiledInterceptor::propertiesToInjectToConstructor() as  $type => $name) {
                     $preference = isset($config['preferences'][$type]) ? $config['preferences'][$type] : $type;
                     foreach ($arguments as $argument) {
-                        if (isset($argument['_i_']) && $argument['_i_'] == $preference) {
-                            continue 2;
+                        if (isset($argument['_i_'])) {
+                            //get real type of argument (for cases where there is a virtual type set for injected argument)
+                            $argumentType = isset($config['instanceTypes'][$argument['_i_']]) ? $config['instanceTypes'][$argument['_i_']] : $argument['_i_'];
+                            if ($argumentType == $preference) {
+                                continue 2;
+                            }
                         }
                     }
                     $arguments = array_merge([$name => ['_i_' => $preference]], $arguments);
@@ -47,7 +37,6 @@ class CompiledInterceptorSubstitution implements ModificationInterface
 
             }
         }
-
         return $config;
     }
 }
