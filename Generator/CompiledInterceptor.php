c524b648e7c6cd9ca29333eea66c52f510ae0c49
Melinda Humphrey <melindashumphrey@gmail.com>
2025-04-11 13:53:00 -0500

    #14 - fix: check for any disabled scopes to exclude when plugin is global

--- Generator/CompiledInterceptor.php
+++ Generator/CompiledInterceptor.php
@@ -712,6 +712,18 @@ class CompiledInterceptor extends EntityAbstract
     private function getScopeCasesFromConfig($config)
     {
         $cases = [];
+
+        //if global scope is enabled, check for any disabled scopes to exclude
+        if (array_key_exists('global', $config)) {
+            $disabledScopes = array_diff_key(
+                $this->areasPlugins->getPluginsConfigForAllAreas(),
+                $config
+            );
+            foreach ($disabledScopes as $scope => $conf) {
+                $config[] = [$scope => []];
+            }
+        }
+
         //group cases by config
         foreach ($config as $scope => $conf) {
             $caseStr = "\tcase '$scope':";
